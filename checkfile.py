1acb13127651f50492b867db37dd343ba25778dc
Andy <Andy@host-155-246-171-12.dhcp.stevens-tech.edu>
2015-04-20 17:02:54 -0400

    Using os.path.realpath(__file__) to replace config path

--- checkfile.py
+++ checkfile.py
@@ -8,15 +8,15 @@ from pync import Notifier
 
 M = imaplib.IMAP4_SSL("imap.gmail.com", 993)
 
-
+path = os.path.split(os.path.realpath(__file__))[0]
 
 M.login(mail.mailbox,mail.password) 
 status, count = M.status("Inbox", "(MESSAGES UNSEEN)")
 unread = count[0].split()[4][:-1]
 if int(unread) == 0:
-	Notifier.notify("No unread mails.", title= mail.mailbox ,appIcon="%sicon.ico"%mail.path)
+	Notifier.notify("No unread mails.", title= mail.mailbox ,appIcon="%s/icon.ico"%mail.path)
 else:
-	Notifier.notify("You have %s mail(s) unread."%str(unread), title=mail.mailbox , appIcon="%sicon.ico"%mail.path ,sound="Ping")
+	Notifier.notify("You have %s mail(s) unread."%str(unread), title=mail.mailbox , appIcon="%s/icon.ico"%mail.path ,sound="Ping")
 
 M.logout()
 
--- mail.py
+++ mail.py
@@ -1,3 +1,2 @@
 mailbox = ""
 password = ""
-path = ""
